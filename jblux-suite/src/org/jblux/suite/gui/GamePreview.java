72cbc25e447594be94c6b0b51762bb759ea38eeb
Casey Jones <jonescaseyb@gmail.com>
2010-04-02 23:37:50 -0500

    suite: Add GamePreview

---
+++ jblux-suite/src/org/jblux/suite/gui/GamePreview.java
@@ -0,0 +1,58 @@
+/**
+ * File: GamePreview.java
+ *
+ * @author Casey Jones
+ *
+ * This file is part of JBlux
+ * JBlux is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * JBlux is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.jblux.suite.gui;
+
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.tiled.TiledMap;
+
+public class GamePreview extends BasicGame {
+    private TiledMap map;
+
+    public GamePreview(String file) {
+        super("JBlux Editor Suite");
+
+        setMap(file);
+    }
+
+    public void setMap(String file) {
+        try {
+            this.map = new TiledMap(file);
+        } catch (SlickException ex) {
+        }
+    }
+
+    @Override
+    public void init(GameContainer gc) throws SlickException {
+    }
+
+    @Override
+    public void update(GameContainer gc, int i) throws SlickException {
+    }
+
+    public void render(GameContainer gc, Graphics grphcs) throws SlickException {
+        map.render(0, 0, 0);
+        map.render(0, 0, 1);
+        map.render(0, 0, 2);
+    }
+}
