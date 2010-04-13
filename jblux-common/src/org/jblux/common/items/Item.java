8ad69e2e4955ef0eec7398b708539d3e1ca3f7c1
Casey Jones <jonescaseyb@gmail.com>
2010-04-12 22:07:48 -0500

    invetory: use field names to match DB

--- jblux-common/src/org/jblux/common/items/Item.java
+++ jblux-common/src/org/jblux/common/items/Item.java
@@ -20,10 +20,72 @@
 
 package org.jblux.common.items;
 
-public interface Item {
-    public String getName();
-    public ItemType getType();
-    public String getImage();
-    public void use();
-    public void destroy();
+public class Item {
+    public int id;
+    public short classs;
+    public short subclass;
+    public String name;
+    public String image;
+    public String description;
+    public int sellPrice;
+    public int buyPrice;
+    public short allowableClass;
+    public short allowableRace;
+    public short itemLevel;
+    public short requiredLevel;
+    public short requiredFaction;
+    public short requiredFactionStanding;
+    public short isUnique;
+    public short maxStack;
+    public short itemStatsCount;
+    public short stat_type1;
+    public short stat_value1;
+    public short stat_type2;
+    public short stat_value2;
+    public short stat_type3;
+    public short stat_value3;
+    public short stat_type4;
+    public short stat_value4;
+    public short stat_type5;
+    public short stat_value5;
+    public short stat_type6;
+    public short stat_value6;
+    public short stat_type7;
+    public short stat_value7;
+    public short stat_type8;
+    public short stat_value8;
+    public short stat_type9;
+    public short stat_value9;
+    public short stat_type10;
+    public short stat_value10;
+    public float dmg_min;
+    public float dmg_max;
+    public short dmg_type;
+    public short armor;
+    public short water_resistance;
+    public short fire_resistance;
+    public short nature_resistance;
+    public short frost_resistance;
+    public short shadow_resistance;
+    public short arcane_resistance;
+    public short delay;
+    public short ammo_type;
+    public float range;
+    public short spellid;
+    public short spellTrigger;
+    public short spellCharges;
+    public short spellCooldown;
+    public short quest_item;
+    public short quest_id;
+    public short block_amount;
+    public short itemset;
+    public short maxDurability;
+    public short requiredDisenchantSkill;
+    public short armorDamageModifier;
+
+    public void use() {
+    }
+    
+    public void destroy() {
+    }
 }
