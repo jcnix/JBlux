0ed284c48a108a9cc55768c01b60cc6c9f5cc294
Casey Jones <jonescaseyb@gmail.com>
2010-07-30 20:27:05 -0500

    django: add new Models to Admin panel

--- jblux_django/jblux/admin.py
+++ jblux_django/jblux/admin.py
@@ -1,6 +1,18 @@
 from jblux_django.jblux.models import User, Character, Inventory, Item, Map
+from jblux_django.jblux.models import MapItems, MapNpcs, Npc, Quest
 from jblux_django.jblux.models import Race, Class, Poll, Choice
 from django.contrib import admin
+
+class MapItemInline(admin.TabularInline):
+    model = MapItems
+    extra = 0
+
+class MapNpcInline(admin.TabularInline):
+    model = MapNpcs
+    extra = 0
+
+class MapAdmin(admin.ModelAdmin):
+    inlines = [MapItemInline, MapNpcInline]
 
 class ChoiceInline(admin.TabularInline):
     fields = ['choice']
@@ -15,7 +27,9 @@ admin.site.register(User)
 admin.site.register(Character)
 admin.site.register(Inventory)
 admin.site.register(Item)
-admin.site.register(Map)
+admin.site.register(Map, MapAdmin)
+admin.site.register(Npc)
+admin.site.register(Quest)
 admin.site.register(Race)
 admin.site.register(Class)
 admin.site.register(Poll, PollAdmin)
