d5ebc9aeb85f489472ff2c963b3dc2caedb7409a
Casey Jones <jonescaseyb@gmail.com>
2010-08-26 23:02:32 -0500

    server: implement more db functions

--- new-server/src/db_user_tbl.c
+++ new-server/src/db_user_tbl.c
@@ -23,16 +23,14 @@ int db_authenticate(char* username, char* password, char* character_name)
     }
     else
     {
-        int id_column = PQfnumber(res, "id");
-        char* cid = PQgetvalue(res, 0, id_column);
-        id = atoi(cid);
+        id = atoi(PQgetvalue(res, 0, 0));
         PQclear(res);
         
         q = "SELECT id FROM jblux_character WHERE name=$1 AND user_id=$2;";
         nParams = 2;
         
         /* Lets use glibc's asprintf because it's safer than sprintf */
-        cid = NULL;
+        char* cid = NULL;
         if(asprintf(&cid, "%d", id) < 0)
         {
             db_disconnect(conn);
@@ -80,13 +78,78 @@ void db_set_map_for_player(int char_id, int map_id, struct coordinates_t coords)
 
 int get_map_for_player(char* character)
 {
+    int map_id = -1;
+    PGconn *conn = db_connect();
+    PGresult *res;
+
+    char* q = "SELECT current_map_id FROM jblux_character WHERE name=$1;";
+    int nParams = 1;
+    const char* params[1] = { character };
+    res = db_exec(conn, q, nParams, params);
+    map_id = atoi(PQgetvalue(res, 0, 0));
+
+    PQclear(res);
+    db_disconnect(conn);
+    return map_id;
 }
 
 struct race_t get_race(int id)
 {
+    struct race_t race;
+    race.id = id;
+    PGconn *conn = db_connect();
+    PGresult *res;
+
+    char* q = "SELECT * FROM jblux_race WHERE id=$1;";
+    int nParams = 1;
+    char *cid;
+    if(asprintf(&cid, "%d", id) < 0)
+    {
+        db_disconnect(conn);
+        race.name = "";
+        race.sprite_sheet = "";
+        race.sprite_height = 0;
+        return race;
+    }
+    const char* params[1] = { cid };
+    res = db_exec(conn, q, nParams, params);
+
+    int column = PQfnumber(res, "name");
+    race.name = PQgetvalue(res, 0, column);
+    column = PQfnumber(res, "sprite_sheet");
+    race.sprite_sheet = PQgetvalue(res, 0, column);
+    column = PQfnumber(res, "sprite_sheet");
+    race.sprite_height = atoi(PQgetvalue(res, 0, column));
+
+    PQclear(res);
+    db_disconnect(conn);
+    return race;
 }
 
 struct class_t get_class(int id)
 {
+    struct class_t class;
+    class.id = id;
+    PGconn *conn = db_connect();
+    PGresult *res;
+
+    char* q = "SELECT * FROM jblux_class WHERE id=$1;";
+    int nParams = 1;
+    char *cid;
+    if(asprintf(&cid, "%d", id) < 0)
+    {
+        db_disconnect(conn);
+        class.name = "";
+        return class;
+    }
+    const char* params[1] = { cid };
+    res = db_exec(conn, q, nParams, params);
+
+    int column = PQfnumber(res, "name");
+    class.name = PQgetvalue(res, 0, column);
+
+    PQclear(res);
+    db_disconnect(conn);
+    return class;
 }
 
