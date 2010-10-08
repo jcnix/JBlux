130ea6250ea16f2a99ec92c8ffa4e18113d231e4
Casey Jones <jonescaseyb@gmail.com>
2010-10-08 12:50:41 -0500

    django: add search_field to some AdminModels

--- jblux_django/jblux/admin.py
+++ jblux_django/jblux/admin.py
@@ -3,6 +3,21 @@ from jblux_django.jblux.models import MapItems, MapNpcs, Npc, Quest
 from jblux_django.jblux.models import Race, Class, Poll, Choice
 from django.contrib import admin
 
+class UserAdmin(admin.ModelAdmin):
+    search_fields = ['username']
+
+class CharacterAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+
+class NpcAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+
+class QuestAdmin(admin.ModelAdmin):
+    search_fields = ['name', 'npc__name']
+
+class ItemAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+
 class MapItemInline(admin.TabularInline):
     model = MapItems
     extra = 0
@@ -12,6 +27,7 @@ class MapNpcInline(admin.TabularInline):
     extra = 0
 
 class MapAdmin(admin.ModelAdmin):
+    search_fields = ['name']
     inlines = [MapItemInline, MapNpcInline]
 
 class ChoiceInline(admin.TabularInline):
@@ -23,13 +39,13 @@ class PollAdmin(admin.ModelAdmin):
     fields = ['pub_date', 'question']
     inlines = [ChoiceInline]
 
-admin.site.register(User)
-admin.site.register(Character)
+admin.site.register(User, UserAdmin)
+admin.site.register(Character, CharacterAdmin)
 admin.site.register(Inventory)
-admin.site.register(Item)
+admin.site.register(Item, ItemAdmin)
 admin.site.register(Map, MapAdmin)
-admin.site.register(Npc)
-admin.site.register(Quest)
+admin.site.register(Npc, NpcAdmin)
+admin.site.register(Quest, QuestAdmin)
 admin.site.register(Race)
 admin.site.register(Class)
 admin.site.register(Poll, PollAdmin)
