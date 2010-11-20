5302a6fd7ea0473f0c12a259a19a70fe2bf528f7
Casey Jones <jonescaseyb@gmail.com>
2010-11-19 18:47:29 -0600

    Fix up errors in npc aggro

--- jblux-server/src/world.c
+++ jblux-server/src/world.c
@@ -11,9 +11,9 @@ static pthread_mutex_t dead_npcs_mutex = PTHREAD_MUTEX_INITIALIZER;
 static struct npc_list *aggro_npcs;
 static pthread_mutex_t aggro_npcs_mutex = PTHREAD_MUTEX_INITIALIZER;
 
-
 static void respawn_npcs();
 static void npcs_attack_target();
+static void remove_aggro_npc(struct npc_data *npc);
 
 void* init_world()
 {
@@ -50,7 +50,7 @@ void respawn_npcs()
         printf("npc: %d\n", npc->npc_id);
         npc->hp = npc->max_hp;
         /* De-aggro the NPC  */
-        npc-> target = NULL;
+            remove_aggro_npc(npc);
 
         char* npc_json = npc_to_json(npc);
         char* command = NULL;
@@ -91,10 +91,44 @@ void npcs_attack_target()
         tell_all_players_on_map(0, npc->map_id, command);
         free(command);
 
-        if(player->hp >= 0) {
+        if(player->hp <= 0) {
             printf("player died\n");
+            remove_aggro_npc(npc);
             player->hp = player->max_hp;
         }
+
+        curr = curr->next;
+    }
+}
+
+void remove_aggro_npc(struct npc_data *npc)
+{
+    struct npc_list *curr = aggro_npcs;
+    struct npc_list *prev = NULL;
+    while(curr)
+    {
+        /* We're only going to determine equality by socket */
+        if(curr->npc->unique_id == npc->unique_id)
+        {
+            pthread_mutex_lock(&aggro_npcs_mutex);
+            if(prev == NULL)
+            {
+                /* We're at the head of the list */
+                aggro_npcs = curr->next;
+            }
+            else
+            {
+                prev->next = curr->next;
+            }
+           
+            struct npc_data *npc = curr->npc;
+            npc->target = NULL;
+            free(curr);
+            pthread_mutex_unlock(&aggro_npcs_mutex);
+            return;
+        }
+        prev = curr;
+        curr = curr->next;
     }
 }
 
