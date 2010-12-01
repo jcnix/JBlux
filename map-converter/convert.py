442cb14e8937b31bdf8854157aa58d1dea506b1f
Casey Jones <jonescaseyb@gmail.com>
2010-11-30 19:55:39 -0600

    Should have been little endian instead of big endian.

--- map-converter/convert.py
+++ map-converter/convert.py
@@ -17,8 +17,8 @@ out = open("output/" + out_name, "wb")
 max_x = im.size[0]
 max_y = im.size[1]
 
-mx = struct.pack('>i', max_x)
-my = struct.pack('>i', max_y)
+mx = struct.pack('i', max_x)
+my = struct.pack('i', max_y)
 out.write(mx)
 out.write(my)
 
